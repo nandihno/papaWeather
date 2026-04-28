4ece9c9720cb624cdae09591d1664582abf3afc8
Nando <fernando.deleon@gmail.com>
2026-04-28 15:58:35 +1000

    new release ....

---
+++ papaWeather/papaWeather/papaWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  papaWeatherApp.swift
+//  papaWeather
+//
+//  Created by Fernando De Leon on 28/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct papaWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
